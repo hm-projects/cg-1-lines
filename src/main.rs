9b592cef8a419926949cb3213c95f21579edb208
anto <antonino.grasso.it@gmail.com>
2023-05-10 19:15:25 +0000

    refactor: optimization

--- src/main.rs
+++ src/main.rs
@@ -12,16 +12,20 @@ fn overlap_for_colinear(p1: &Point, p2: &Point, q1: &Point, q2: &Point) -> bool 
     let q1x_in_px = q1.x >= p1.x.min(p2.x) && q1.x <= p1.x.max(p2.x);
     let q2x_in_px = q2.x >= p2.x.min(p2.x) && q2.x <= p2.x.max(p2.x);
 
+    let x_test = q1x_in_px || q2x_in_px;
+
+    if !x_test {
+        // early return
+        return false;
+    }
+
     // y
     let q1y_in_py = q1.y >= p1.y.min(p2.y) && q1.y <= p1.y.max(p2.y);
     let q2y_in_py = q2.y >= p2.y.min(p2.y) && q2.y <= p2.y.max(p2.y);
 
+    let y_test = q1y_in_py || q2y_in_py;
 
-    if (q1x_in_px || q2x_in_px) && (q1y_in_py || q2y_in_py) {
-        return true
-    }
-
-    return false;
+    x_test && y_test
 }
 
 fn intersect(p1: &Point, p2: &Point, q1: &Point, q2: &Point) -> bool {
@@ -49,13 +53,7 @@ use std::env;
 use std::time::Instant;
 use std::{fs, vec};
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let path = &args[1];
-
-    // read csv file
-    let s = fs::read_to_string(path).expect("cannot read file");
-
+fn run(s: &String, intersection_test: fn(&Point, &Point, &Point, &Point) -> bool) {
     let points: Vec<Vec<Point>> = s
         .lines()
         .into_iter()
@@ -82,7 +80,7 @@ fn main() {
     let mut i: usize = 0;
     for line in points.iter() {
         for other_line in points.iter().skip(i + 1) {
-            let b = intersect(&line[0], &line[1], &other_line[0], &other_line[1]);
+            let b = intersection_test(&line[0], &line[1], &other_line[0], &other_line[1]);
             if b {
                 count += 1
             }
@@ -93,6 +91,17 @@ fn main() {
 
     println!("intersecting lines: {}", count);
     println!("elapsed time: {:.4?}", elapsed);
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let path = &args[1];
+
+    // read csv file
+    let s = fs::read_to_string(path).expect("cannot read file");
+
+    println!(">>>>>>Normal<<<<<<");
+    run(&s, intersect);
 }
 
 #[cfg(test)]
@@ -127,6 +136,16 @@ mod tests {
     #[test]
     fn test_basic_cross() {
         assert_intersect((1.0, 0.0, 1.0, 2.0), (0.0, 1.0, 2.0, 1.0), true);
+    }
+
+    #[test]
+    fn test_basic_cross_swapped() {
+        assert_intersect((0.0, 1.0, 2.0, 1.0), (1.0, 0.0, 1.0, 2.0), true);
+    }
+
+    #[test]
+    fn test_basic_cross_inverted() {
+        assert_intersect((0.0, 1.0, 2.0, 1.0), (1.0, 0.0, 1.0, 2.0), true);
     }
 
     #[test]
