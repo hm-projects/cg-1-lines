4fe0e079bc07a389e9325e7824d0022aa2bfbb0d
anto <antonino.grasso.it@gmail.com>
2023-06-30 13:25:08 +0200

    test: add case

--- src/main.rs
+++ src/main.rs
@@ -209,6 +209,11 @@ mod tests {
     }
 
     #[test]
+    fn test_parallel2() {
+        assert_intersect((2.0, 5.0, 5.0, 4.0), (3.0, 2.0, 6.0, 1.0), false);
+    }
+
+    #[test]
     fn test_colinear_but_common_part() {
         assert_intersect((0.0, 0.0, 2.0, 0.0), (1.0, 0.0, 3.0, 0.0), true);
     }
